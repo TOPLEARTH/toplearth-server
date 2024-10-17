7d76b43a8ee2d40df4f7ea127ed4a7ddab0c28ba
jun6292 <c68254@gmail.com>
2024-10-18 05:18:06 +0900

    :tada: 전역 상수 관리 클래스 추가

---
+++ src/main/java/com/gdsc/toplearth_server/core/constant/Constants.java
@@ -0,0 +1,13 @@
+package com.gdsc.toplearth_server.core.constant;
+
+import java.util.List;
+
+public final class Constants {
+    public static final String BEARER_PREFIX = "Bearer ";
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    // 인증이 필요 없는 URL
+    public static List<String> NO_NEED_AUTH_URLS = List.of(
+
+    );
+}
