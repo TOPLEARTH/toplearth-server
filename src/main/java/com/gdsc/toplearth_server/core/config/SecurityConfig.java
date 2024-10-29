0d247664649c28bce9272919461f37aa86ba8d75
park20011029 <rokmck156@gmail.com>
2024-10-29 20:54:23 +0900

    Feat: Config filter 설정 및 exception 설정

--- src/main/java/com/gdsc/toplearth_server/core/config/SecurityConfig.java
+++ src/main/java/com/gdsc/toplearth_server/core/config/SecurityConfig.java
@@ -1,6 +1,12 @@
 package com.gdsc.toplearth_server.core.config;
 
 import com.gdsc.toplearth_server.core.constant.Constants;
+import com.gdsc.toplearth_server.core.security.filter.JwtAuthenticationFilter;
+import com.gdsc.toplearth_server.core.security.filter.JwtExceptionFilter;
+import com.gdsc.toplearth_server.core.security.handler.JwtAccessDeniedHandler;
+import com.gdsc.toplearth_server.core.security.handler.JwtEntryPoint;
+import com.gdsc.toplearth_server.core.security.service.CustomUserDetailService;
+import com.gdsc.toplearth_server.core.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,12 +16,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
+    private final CustomUserDetailService customUserDetailService;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtEntryPoint jwtEntryPoint;
+    private final JwtUtil jwtUtil;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -26,10 +38,20 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers(Constants.NO_NEED_AUTH_URLS.toArray(new String[0])).permitAll()
+                                .requestMatchers(Constants.NO_NEED_AUTH_URLS.toArray(String[]::new)).permitAll()
                                 .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
+                .exceptionHandling((exceptionHandling) ->
+                        exceptionHandling
+                                .authenticationEntryPoint(jwtEntryPoint)
+                                .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(customUserDetailService, jwtUtil),
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(
+                        new JwtExceptionFilter(),
+                        JwtAuthenticationFilter.class)
                 .build();
     }
 }
