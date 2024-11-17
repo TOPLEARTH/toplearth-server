a99dc88e9b542613513da9e8b024d8037e35965c
park20011029 <rokmck156@gmail.com>
2024-11-17 23:22:21 +0900

    Feat: 유저 목표 수정

---
+++ src/main/java/com/gdsc/toplearth_server/presentation/controller/user/UserContoller.java
@@ -0,0 +1,30 @@
+package com.gdsc.toplearth_server.presentation.controller.user;
+
+import com.gdsc.toplearth_server.application.dto.user.UpdateGoalResponseDto;
+import com.gdsc.toplearth_server.application.service.user.UserService;
+import com.gdsc.toplearth_server.core.annotation.UserId;
+import com.gdsc.toplearth_server.core.common.CommonResponseDto;
+import com.gdsc.toplearth_server.presentation.request.user.UpdateGoalRequestDto;
+import jakarta.validation.Valid;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserContoller {
+    private final UserService userService;
+
+    @PatchMapping("/goal")
+    public CommonResponseDto<UpdateGoalResponseDto> updateGole(
+            @Valid @RequestBody UpdateGoalRequestDto updateGoalRequestDto,
+            @UserId UUID userId
+    ) {
+        return CommonResponseDto.ok(userService.updateGoal(updateGoalRequestDto, userId));
+    }
+
+}
