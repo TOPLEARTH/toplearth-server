2025ed3c381de57faff9cfec8c5e10b794dee2db
park20011029 <rokmck156@gmail.com>
2024-11-19 04:15:13 +0900

    Test: 임시 admin 계정 생성 api

--- src/main/java/com/gdsc/toplearth_server/presentation/controller/TestController.java
+++ src/main/java/com/gdsc/toplearth_server/presentation/controller/TestController.java
@@ -4,11 +4,14 @@ import com.gdsc.toplearth_server.application.service.TestService;
 import com.gdsc.toplearth_server.core.annotation.UserId;
 import com.gdsc.toplearth_server.core.common.CommonResponseDto;
 import com.gdsc.toplearth_server.domain.entity.user.type.EUserRole;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/test")
@@ -28,6 +31,14 @@ public class TestController {
     ) {
         log.info("name: {}", name);
         return CommonResponseDto.ok(testService.signIn(name, EUserRole.USER));
+    }
+
+    @PostMapping("/signin/admin/{name}")
+    public CommonResponseDto<?> signInAdmin(
+            @PathVariable("name") String name
+    ) {
+        log.info("name: {}", name);
+        return CommonResponseDto.ok(testService.signInAdmin(name, EUserRole.ADMIN));
     }
 
     @GetMapping("")
